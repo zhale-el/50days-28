45c2ec83a144afde1d24e7fbca7821aa4b876267
zhaleh <zhalehelmi@gmail.com>
2023-11-10 12:04:36 +0330

    add function getUser

---
+++ script.js
@@ -0,0 +1,10 @@
+const APIURL = "https://api.github.com/users/";
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
